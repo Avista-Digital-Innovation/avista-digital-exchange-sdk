f9a3488a91956f34954093eba5296b694f396582
justinwhicker <jwhicker@urbanova.org>
2022-09-12 16:15:43 -0700

    initial commit

---
+++ avista_digital_exchange/src/avista_digital_exchange/__init__.py
@@ -0,0 +1,3 @@
+from .avista_digital_exchange import AvistaDigitalExchange
+from .graphql_types import *
+from .graphql_queries import *
---
+++ avista_digital_exchange/src/avista_digital_exchange/common.py
@@ -0,0 +1,10 @@
+
+debug = True
+
+
+tab = "    "
+def getTabStr(tabs = 1):
+    tabStr = ""
+    for i in range(tabs):
+        tabStr += tab
+    return tabStr
