63b817f2541b6516991bc3feae12df35d9d17cd1
Justin Whicker <justin.m.whicker@gmail.com>
2023-07-18 14:47:58 -0700

    fixing submodule

--- graphql_codegen/step_2_ariadne/generate.sh
+++ graphql_codegen/step_2_ariadne/generate.sh
@@ -1,7 +1,7 @@
 FILE=../step_1_gqlg/output/queries.graphql
 if [ -f "$FILE" ]; then
     echo "$FILE exists. Running ariadne-codegen command.";
-    ariadne-codegen;
+    # ariadne-codegen;
 else 
     echo "$FILE does not exist. You must first run step_1_gqlg/generate.sh. This should generate files in directory ../step1_gqlg/output/";
     exit 1
